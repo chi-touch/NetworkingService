6aed671b0117324671840397bbf3ea05c56f4185
chi-touch <gloebudav@gmail.com>
2024-11-13 14:30:07 +0100

    first commit

---
+++ src/main/java/networking/NetworkingServiceApplication.java
@@ -0,0 +1,13 @@
+package networking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingServiceApplication.class, args);
+	}
+
+}
